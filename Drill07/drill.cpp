9c80a1bbe661a304b95740f9d406f8c197af4d45
Kovács Bence <kovacsbencedaniel2@gmail.com>
2022-04-08 00:04:31 +0200

    Finished Drill07

--- Drill07/drill.cpp
+++ Drill07/drill.cpp
@@ -1,82 +1,84 @@
-//7. drill
-#include "../std_lib_facilities.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+template <typename Iterator1, typename Iterator2>
+void Write(Iterator1 array_begin, Iterator2 array_end)
+{
+   for (auto i = array_begin; i!=array_end; ++i)
+   {
+       cout << *i <<" ";
+   }
+   cout << endl;
+}
+
+template<typename Iterator1, typename Iterator2>
+Iterator2 Copy(Iterator1 f1, Iterator1 e1, Iterator2 f2)
+{
+    for (Iterator1 i=f1; i!=e1; ++i)
+    {
+        *f2++=*i;
+    }
+    return f2;
+}
+
+template<typename T>
+void Add(T& array, int amount)
+{
+    for (auto& i:array)
+    {
+        i+=amount;
+    }
+    
+}
 
 int main()
-{	
-	//1.
-	int aint [10] = {0,1,2,3,4,5,6,7,8,9};
-	//2.
-	vector<int> vint;
-	//3.
-	list<int> lint;
-	
-	for (int i=0; i<10; i++)
-	{
-		vint.push_back(i);
-		lint.push_back(i);
-	}
+{
+    array<int,10> aint = {0,1,2,3,4,5,6,7,8,9};
+    vector<int> vint = {0,1,2,3,4,5,6,7,8,9};
+    list<int> lint = {0,1,2,3,4,5,6,7,8,9};
 
-	//copies
-	int aint2 [10];
-	vector<int> vint2;
-	list<int> lint2;
+    array<int,10> aint1 = aint;
+    vector<int> vint1 = vint;
+    list<int> lint1 = lint;
+    
+    cout << "Array: ";
+    Write(aint.begin(),aint.end());
+    cout << "Vector: ";
+    Write(vint.begin(),vint.end());
+    cout << "List: ";
+    Write(lint.begin(),lint.end());
+    cout << endl;
+  
+    Add(aint,2);
+    Add(vint,3);
+    Add(lint,5);
 
-	//write out
-	for (auto i = 0; i < 10; ++i)
-	{
-        	cout << aint[i] << " ";
-        }
-	cout << "\n";
-	for (auto i = vint.begin(); i != vint.end(); ++i)
-	{
-        	cout << *i << " ";
-        }
-        cout << "\n";
-        for (auto i = lint.begin(); i != lint.end(); ++i)
-        {
-        	cout << *i << " ";
-	}   
-	cout << "\n";
+    cout << "Array: ";
+    Write(aint.begin(),aint.end());
+    cout << "Vector: ";
+    Write(vint.begin(),vint.end());
+    cout << "List: ";
+    Write(lint.begin(),lint.end());
+    cout << endl;
 
-	//5.
-	//ELEMENT CHANGE
-	//array increase value by 2
-	for (auto i = 0; i < 10; ++i)
-	{
-        	aint[i]=aint[i]+2;
-        }
-	cout << "\n";
-	//vector increase value by 3
-	for (auto i = 0; i < 10; ++i)
-	{
-        	vint[i]=vint[i]+3;
-        }
-	cout << "\n";
-	//list increase value by 5
-	
-	
-	
-	
-	
-	
-	
-	//write out
-	for (auto i = 0; i < 10; ++i)
-	{
-        	cout << aint[i] << " ";
-        }
-	cout << "\n";
-	for (auto i = vint.begin(); i != vint.end(); ++i)
-	{
-        	cout << *i << " ";
-        }
-        cout << "\n";
-	for (auto i = lint.begin(); i != lint.end(); ++i)
-        {
-        	cout << *i << " ";
-	}   
-	cout << "\n";
+    Copy(aint.begin(),aint.end(),vint.begin());
 
+    vector<int>::iterator ivint = find(vint.begin(),vint.end(),3);
+    if (ivint != vint.end())
+    {
+        cout << "A 3-as a(z) " << distance(vint.begin(),ivint) << " helyen van\n";
+    }
+    else cout << "Nincs 3-as" << endl;
 
-	return 0;
+    list<int>::iterator ilint = find(lint.begin(),lint.end(),27);
+    if (ilint != lint.end())
+    {
+        cout << "A 27 a(z) " << distance(lint.begin(),ilint) << " helyen van";
+    }
+    else cout << "Nincs 27-es" << endl;
 }
